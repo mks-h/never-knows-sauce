5f1a3eed1fde1975cc1bbf539fd1c6a01f7d3bdb
Maksym Hazevych <mhazevych@proton.me>
2025-04-14 22:12:21 +0300

    Use tabs for indentation

--- app.ts
+++ app.ts
@@ -3,56 +3,56 @@ import { AppConfig } from "./config/index.ts";
 import { ILocalizationService } from "./services/index.ts";
 
 export class Application {
-  private readonly appConfig: AppConfig;
-  private readonly logger: Logger;
-  private readonly localizationService: ILocalizationService;
+	private readonly appConfig: AppConfig;
+	private readonly logger: Logger;
+	private readonly localizationService: ILocalizationService;
 
-  constructor(
-    appConfig: AppConfig,
-    logger: Logger,
-    localizationService: ILocalizationService,
-  ) {
-    this.appConfig = appConfig;
-    this.logger = logger;
-    this.localizationService = localizationService;
-  }
+	constructor(
+		appConfig: AppConfig,
+		logger: Logger,
+		localizationService: ILocalizationService,
+	) {
+		this.appConfig = appConfig;
+		this.logger = logger;
+		this.localizationService = localizationService;
+	}
 
-  public start(): void {
-    //TODO: app initialization
-    this.logger.info`Application has been started.`;
+	public start(): void {
+		//TODO: app initialization
+		this.logger.info`Application has been started.`;
 
-    this.logger.info`${this.localizationService.getTranslation("hello.world")}`;
-    this.logger.info`${
-      this.localizationService.getTranslation(
-        "hello.world",
-        {},
-        "en",
-      )
-    }`;
-    this.logger.info`${
-      this.localizationService.getTranslation(
-        "hello.world",
-        {},
-        "ua",
-      )
-    }`;
+		this.logger.info`${this.localizationService.getTranslation("hello.world")}`;
+		this.logger.info`${
+			this.localizationService.getTranslation(
+				"hello.world",
+				{},
+				"en",
+			)
+		}`;
+		this.logger.info`${
+			this.localizationService.getTranslation(
+				"hello.world",
+				{},
+				"ua",
+			)
+		}`;
 
-    // templates
+		// templates
 
-    this.logger.info`${this.localizationService.getTranslation("greetings")}`;
-    this.logger.info`${
-      this.localizationService.getTranslation(
-        "greetings",
-        { userName: "Maksym" },
-        "en",
-      )
-    }`;
-    this.logger.info`${
-      this.localizationService.getTranslation(
-        "greetings",
-        { userName: "Максим" },
-        "ua",
-      )
-    }`;
-  }
+		this.logger.info`${this.localizationService.getTranslation("greetings")}`;
+		this.logger.info`${
+			this.localizationService.getTranslation(
+				"greetings",
+				{ userName: "Maksym" },
+				"en",
+			)
+		}`;
+		this.logger.info`${
+			this.localizationService.getTranslation(
+				"greetings",
+				{ userName: "Максим" },
+				"ua",
+			)
+		}`;
+	}
 }
--- services/BaseLocalizationService.ts
+++ services/BaseLocalizationService.ts
@@ -1,18 +1,18 @@
 export class BaseLocalizationService {
-  public applyTemplateValuesToLocalization(
-    localization: string,
-    templateValues?: { [key: string]: string },
-  ): string {
-    if (!templateValues) {
-      return localization;
-    }
+	public applyTemplateValuesToLocalization(
+		localization: string,
+		templateValues?: { [key: string]: string },
+	): string {
+		if (!templateValues) {
+			return localization;
+		}
 
-    let result = localization;
+		let result = localization;
 
-    Object.entries(templateValues).forEach(([key, value]) => {
-      result = result.replaceAll(`{{${key}}}`, value);
-    });
+		Object.entries(templateValues).forEach(([key, value]) => {
+			result = result.replaceAll(`{{${key}}}`, value);
+		});
 
-    return result;
-  }
+		return result;
+	}
 }
