c2cf8cb9ad48f15bb007d0fa1471bff6fa34afee
Maksym Hazevych <mhazevych@proton.me>
2025-05-18 19:54:58 +0300

    Separate class initialization methods into functions
    This prevents from passing uninitialized objects as valid
    values into places that expect an initialized object.

--- src/db/DatabaseClient.ts
+++ src/db/DatabaseClient.ts
@@ -1,47 +1,29 @@
 import mongoose, { Schema } from "npm:mongoose";
 import { Logger } from "@logtape/logtape";
-import { AppConfig } from "../config/index.ts";
+
+export function initDbClient(logger: Logger, connectionString: string) {
+	const connection = mongoose.createConnection(connectionString);
+	logger.info`MongoDb connection was successfully created.`;
+	logger.debug`MongoDb connection string: ${connectionString}`;
+
+	return new DbClient(logger, connection);
+}
 
 export class DbClient {
-	isInitialized: boolean;
-
-	private readonly connectionString: string;
-	private connection: mongoose.Connection | null;
-	private logger: Logger;
-
-	constructor(appConfig: AppConfig, logger: Logger) {
-		this.connectionString = appConfig.DbConnectionString;
-		this.isInitialized = false;
-		this.connection = null;
-		this.logger = logger;
-	}
+	constructor(
+		private logger: Logger,
+		private connection: mongoose.Connection,
+	) {}
 
 	getCollectionModel<T>(
 		name: string,
 		schema: Schema,
 	): mongoose.Model<T> {
-		if (!this.isInitialized || !this.connection) {
-			throw new Error(
-				"Try to get MongoDb collection before client initialization",
-			);
-		}
-
 		return this.connection.model<T>(name, schema);
 	}
 
-	initialize() {
-		if (!this.isInitialized) {
-			this.connection = mongoose.createConnection(this.connectionString);
-			this.logger.info`MongoDb connection was successfully created.`;
-			this.logger.debug`MongoDb connection string: ${this.connectionString}`;
-			this.isInitialized = true;
-		}
-	}
-
 	async dispose() {
-		if (this.isInitialized) {
-			this.logger.info`MongoDB connection was closed`;
-			await this.connection?.close();
-		}
+		this.logger.info`MongoDB connection was closed`;
+		await this.connection.close();
 	}
 }
